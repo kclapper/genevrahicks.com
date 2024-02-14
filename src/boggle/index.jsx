94e71c173e40d2340d8bbaf4991dda3edcf37fa4
Kyle Clapper <35474281+kclapper@users.noreply.github.com>
2024-02-13 22:34:06 -0500

    Valentines Day

--- src/boggle/index.jsx
+++ src/boggle/index.jsx
@@ -1,12 +1,12 @@
 import { StrictMode, useState, useCallback } from "react";
 import { createRoot } from 'react-dom/client';
 
+import { Container, Row, Col } from "react-bootstrap";
+
 import { Boggle } from 'koggle';
 
 import PageContent from '../components/PageContent';
 import { defaultSettings, Settings } from '../components/Settings';
-
-import "../Home.scss";
 
 export function Home() {
   const [variant, setVariant] = useState(defaultSettings.variant);
@@ -16,19 +16,29 @@ export function Home() {
   }, [setVariant]);
 
   return <StrictMode>
-            <PageContent>
-              <Settings onChange={handleChange} />
+            <Container>
+              <Row>
+              <Col className="text-center text-danger display-1 align-middle d-flex align-items-center justify-content-center">
+                ♥
+              </Col>
+              <Col>
+                <Settings onChange={handleChange} />
 
-              <h1 className="display-1 text-center mb-2">
-                Boggle
-              </h1>
+                <h1 className="display-1 text-center mb-2 text-primary">
+                  Boggle
+                </h1>
 
-              <Boggle variant={ variant } />
+                <Boggle variant={ variant } />
 
-              <div className="text-end mt-4 mb-4 fst-italic fs-6 w-75">
-                - Love Kyle
-              </div>
-            </PageContent>
+                <div className="text-end mt-4 mb-4 fst-italic fs-6 w-75">
+                  - Happy Valentines Day baby, I love you
+                </div>
+              </Col>
+              <Col className="text-center text-danger display-1 align-middle d-flex align-items-center justify-content-center">
+                ♥
+              </Col>
+              </Row>
+            </Container>
          </StrictMode>
 }
 
